c25cfbcaa3a3ace8ef7fa984723997d847beafba
Brian Wynn <brian.wynn83@gmail.com>
2016-02-06 17:39:49 -0800

    Updating uri info.

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'database': 'mongodb://localhost/basic-auth'
+  'database': 'mongodb://localhost/basic-auth',
+  'uri': 'mongodb://heroku_hf8wx8j8:jusrkj6pll8f24o2vpt02ms73i@ds059205.mongolab.com:59205/heroku_hf8wx8j8'
 };
